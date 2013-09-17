182e88f5ab1f2062e2cb15a6b99d3a5f7f70d9f5
kenjiskywalker <git@kenjiskywalker.org>
2013-09-17 18:41:07 +0900

    1st

---
+++ temple-kun.go
@@ -0,0 +1,23 @@
+package temple
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+	//	http.ListenAndServe("localhost:9999", nil)
+}
+
+func handler(rw http.ResponseWriter, req *http.Request) {
+
+	pos := strings.LastIndex(req.RemoteAddr, ":")
+	if pos > 0 {
+		addr := strings.Split(req.RemoteAddr, ":")
+		// fmt.Fprintf(rw, "YOUR IP ADDRESS %s", addr[0])
+		fmt.Fprintf(rw, addr[0])
+	}
+
+}
