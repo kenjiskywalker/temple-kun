2dd40bb349e050eebb584d41e7e32b7f63839426
kenjiskywalker <git@kenjiskywalker.org>
2013-09-18 22:33:57 +0900

    1st

---
+++ temple-kun.go
@@ -0,0 +1,15 @@
+package temple
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("localhost:9999", nil)
+}
+
+func handler(rw http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(rw, "%s\n", req.RemoteAddr)
+}
